dcdffe4a017d43357fb241c8233b9389b8943180
NIKESH KUMAR <nikesh.kumar7@ibm.com>
2024-07-15 10:35:13 +0530

    added new feature

--- Hook/Task/useStateConcept/Pagination/UiPagination.jsx
+++ Hook/Task/useStateConcept/Pagination/UiPagination.jsx
@@ -3,15 +3,15 @@ import useLogic from './useLogic'
 
 const UiPagination = () => {
   const Hook = useLogic('https://jsonplaceholder.typicode.com/posts');
-  const { data, setData, pagination, setPagination, page, setPage, isLoading, error } = Hook;
+  const { data, setData, pagination, setPagination, page, setPage, isLoading, error, searchContent, setSearchContent, serchData } = Hook;
 
   const handleRemove = (id) => {
-    const filterData =  data.filter(data=> data.id != id)
+    const filterData = data.filter(data => data.id != id)
     setData(filterData)
   }
 
   const handelChangePage = (e) => {
-    const pageGet =  Number(e.target.value)
+    const pageGet = Number(e.target.value)
     setPagination(pageGet)
     setPage(1)
   }
@@ -23,13 +23,53 @@ const UiPagination = () => {
   if (error) {
     return <p>{error}</p>
   }
+
+  console.log('serchData : ', serchData)
   return (
     <div>
       <h2>Hook With Pagination</h2>
+      <input type="text" placeholder='search me...' value={searchContent} onChange={(e) => setSearchContent(e.target.value)} style={{ padding: '12px 11rem' }} />
+
+
       {data.length === 0 ? 'No Data existing...' : (
         <div>
-          <h2>Welcom To Our Page</h2>
-          {data.map(dataGet => {
+          {/* {serchData.length === 0  && <h2>No filter data found</h2>} */}
+          {serchData.length ? (
+            <div className="">
+              {serchData.map(dataGet => {
+                const { id, title, body } = dataGet;
+
+                return (
+                  <div key={id} style={{
+                    padding: '1px', 
+                    margin: '2rem',
+                  }}>
+                    <h4 style={{color: 'red'}}>{title}</h4>
+                    <p style={{color : 'green', fontFamily: 'sans-serif'}}> {body}</p>
+                    {/* <button onClick={() => handleRemove(id)}>❌</button> */}
+                  </div>
+                )
+              })}
+            </div>
+          ) : (
+            <div className="">
+              {data.map(dataGet => {
+                const { id, title } = dataGet;
+
+                return (
+                  <div key={id} style={{
+                    padding: '1px', border: '1px solid red',
+                    margin: '2rem', display: 'flex',
+                    justifyContent: 'center', alignItems: 'center', gap: '2rem', borderRadius: '50px'
+                  }}>
+                    <p>{title}</p>
+                    <button onClick={() => handleRemove(id)}>❌</button>
+                  </div>
+                )
+              })}
+            </div>
+          )}
+          {/* {data.map(dataGet => {
             const { id, title } = dataGet;
 
             return (
@@ -42,11 +82,11 @@ const UiPagination = () => {
                 <button onClick={()=>handleRemove(id)}>❌</button>
               </div>
             )
-          })}
+          })} */}
           <span>page {page} of 10</span>
           <div className="">
-          <button onClick={()=>setPage(prev=> prev - 1)} disabled = {page === 1}>⬅️</button>
-          <button  onClick={()=>setPage(prev=> prev + 1)} disabled = {page === 10}>➡️</button>
+            <button onClick={() => setPage(prev => prev - 1)} disabled={page === 1}>⬅️</button>
+            <button onClick={() => setPage(prev => prev + 1)} disabled={page === 10}>➡️</button>
           </div>
 
           <div className="">
@@ -59,7 +99,7 @@ const UiPagination = () => {
             </select>
           </div>
 
-          <button onClick={()=> setData([])}>All 🆑</button>
+          <button onClick={() => setData([])}>All 🆑</button>
         </div>
       )}
 
--- Hook/Task/useStateConcept/Pagination/useLogic.jsx
+++ Hook/Task/useStateConcept/Pagination/useLogic.jsx
@@ -5,7 +5,9 @@ const useLogic = (url) => {
   const [pagination, setPagination] = useState(10)
   const [page, setPage] = useState(1);
   const [error, setError] = useState(null);
-  const [isLoading, setLoding] = useState(true)
+  const [isLoading, setLoding] = useState(true);
+  const [searchContent, setSearchContent] = useState('')
+  const [serchData, setSearchData] = useState([])
 
   const fetchApi = async (page, pagination) => {
     try {
@@ -22,17 +24,28 @@ const useLogic = (url) => {
       setData(paginationSlice);
       setLoding(false)
 
+      
+
     } catch (error) {
       setError(error.message)
     }
   }
 
+  const searchMe = () => {
+    if (searchContent != '') {
+      const serachFilter =  data.filter(dataSearch => dataSearch.title.includes(searchContent))
+      // console.log('searchContent : ', serachFilter)
+      setSearchData(serachFilter)
+    }
+  }
+
   useEffect(() => {
     fetchApi(page, pagination)
-  }, [page, pagination])
+    searchMe()
+  }, [page, pagination, searchContent])
 
 
-  return { data, setData, pagination, setPagination, page, setPage, isLoading, error }
+  return { data, setData, pagination, setPagination, page, setPage, isLoading, error, searchContent, setSearchContent, serchData }
 }
 
 export default useLogic
